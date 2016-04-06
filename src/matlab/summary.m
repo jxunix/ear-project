8fe044521fb290e434f4155935faf71b3283142a
Jun Xu <xnirvana508@gmail.com>
2016-04-06 17:27:35 -0400

    Automatic commit of successful build 20160406-17:27:35

---
+++ src/matlab/summary.m
@@ -0,0 +1,101 @@
+%
+% Filename: summary.m
+% Author: Jun Xu
+% Email: junx@cs.bu.edu
+% Created Time: Wed 06 Apr 2016 04:39:25 PM EDT
+% Description: This script summarize all the recognition rates.
+%
+
+clear; close all;
+
+fname = '../../results/knn_ws.mat';
+variables = { 'acc' };
+ws = load(fname, variables{:});
+knn_test_acc = ws.('acc');
+
+fname = '../../results/flda_ws.mat';
+variables = { 'train_acc', 'test_acc' };
+ws = load(fname, variables{:});
+flda_train_acc = ws.('train_acc');
+flda_test_acc = ws.('test_acc');
+
+fname = '../../results/svm_ws.mat';
+variables = { 'train_acc', 'test_acc' };
+ws = load(fname, variables{:});
+svm_train_acc = ws.('train_acc');
+svm_test_acc = ws.('test_acc');
+
+%fname = '../../results/svm_own_ws.mat';
+%variables = { 'train_acc', 'test_acc' };
+%ws = load(fname, variables{:});
+%svm_own_train_acc = ws.('train_acc');
+%svm_own_test_acc = ws.('test_acc');
+
+clear fname variables ws;
+
+knn_test_acc_mean = mean(knn_test_acc, 2);
+knn_test_acc_std = std(knn_test_acc, 0, 2);
+
+flda_train_acc_mean = mean(flda_train_acc, 2);
+flda_test_acc_mean = mean(flda_test_acc, 2);
+flda_train_acc_std = std(flda_train_acc, 0, 2);
+flda_test_acc_std = std(flda_test_acc, 0, 2);
+
+svm_train_acc_mean = mean(svm_train_acc, 2);
+svm_test_acc_mean = mean(svm_test_acc, 2);
+svm_train_acc_std = std(svm_train_acc, 0, 2);
+svm_test_acc_std = std(svm_test_acc, 0, 2);
+
+%svm_own_train_acc_mean = mean(svm_own_train_acc, 2);
+%svm_own_test_acc_mean = mean(svm_own_test_acc, 2);
+%svm_own_train_acc_std = std(svm_own_train_acc, 0, 2);
+%svm_own_test_acc_std = std(svm_own_test_acc, 0, 2);
+
+[ val idx ] = max(knn_test_acc_mean);
+knn_train = 100;
+knn_train_std = 0;
+knn_test = val;
+knn_test_std = knn_test_acc_std(idx);
+
+[ val idx ] = max(flda_train_acc_mean);
+flda_train = val;
+flda_test = flda_test_acc_mean(idx);
+flda_train_std = flda_train_acc_std(idx);
+flda_test_std = flda_test_acc_std(idx);
+
+[ val idx ] = max(svm_train_acc_mean);
+svm_train = val;
+svm_test = svm_test_acc_mean(idx);
+svm_train_std = svm_train_acc_std(idx);
+svm_test_std = svm_test_acc_std(idx);
+
+%[ val idx ] = max(svm_own_train_acc_mean);
+%svm_own_train = val;
+%svm_own_test = svm_own_test_acc_mean(idx);
+%svm_own_train_std = svm_own_train_acc_std(idx);
+%svm_own_test_std = svm_own_test_acc_std(idx);
+
+%train = [ knn_train flda_train svm_train svm_own_train ];
+%test = [ knn_test flda_test svm_test svm_own_test ];
+%train_std = [ knn_train_std flda_train_std svm_train_std svm_own_train_std ];
+%test_std = [ knn_test_std flda_test_std svm_test_std svm_own_test_std ];
+
+train = [ knn_train flda_train svm_train ];
+test = [ knn_test flda_test svm_test ];
+train_std = [ knn_train_std flda_train_std svm_train_std ];
+test_std = [ knn_test_std flda_test_std svm_test_std ];
+
+means = [ train; test ];
+stds = [ train_std; test_std ];
+
+errorbar_groups(means', stds', ...
+	'bar_width', 0.75, ...
+	'errorbar_width', 0.5, ...
+	'bar_names', {'Training', 'Test'});
+%set(gca, 'XTickLabel', { 'KNN', 'FLDA', 'SVM' });
+ylabel('Recognition Rate');
+title('Comparison of Feature Classification Methods');
+legend('KNN', 'FLDA', 'ASM (libsvm)');
+
+outname = '../../results/classification_comparision.png';
+print(outname, '-dpng');
