087a8a59892626547c391677dbeb9606696371a8
jxunix <xnirvana508@gmail.com>
2016-04-06 01:06:17 -0400

    Automatic commit of successful build 20160406-01:06:15

--- src/matlab/extract_sift.m
+++ src/matlab/extract_sift.m
@@ -6,8 +6,8 @@
 % Description: This script is to extract SIFT features from ear images.
 %
 
-%run('../../vlfeat-0.9.20/toolbox/vl_setup.m')
-%vl_version verbose
+run('../../../vlfeat-0.9.20/toolbox/vl_setup.m')
+vl_version verbose
 
 %%
 crv_fname = '../../asm/models/ear_99pts.crvs';
@@ -22,13 +22,13 @@ end
 
 %%
 pt_dname = '../../asm/points_99/';
-fnames = dir(pt_dname);
-fnames = {fnames(3:end).name}';
-xv = zeros(length(fnames), length(polyline));
-yv = zeros(length(fnames), length(polyline));
+pt_fnames = dir(pt_dname);
+pt_fnames = {pt_fnames(3:end).name}';
+xv = zeros(length(pt_fnames), length(polyline));
+yv = zeros(length(pt_fnames), length(polyline));
 
-for i = 1:length(fnames)
-	f = strcat(pt_dname, fnames{i});
+for i = 1:length(pt_fnames)
+	f = strcat(pt_dname, pt_fnames{i});
 	pts = textread(f, '%s', 'delimiter', '\n');
 	pts = pts(4:end-1);
 	pts = pts(polyline + 1);
@@ -42,11 +42,13 @@ end
 
 %%
 dname = '../../outputs/4_flipped/';
+fnames = dir(pt_dname);
+fnames = {fnames(3:end).name}';
 
 % extract SIFT features from the images
 descriptors = cell(length(fnames), 1);
 for i = 1:length(fnames)
-	fname = strcat(dname, fnames{i}(1:end - 3), 'jpg');
+	fname = strcat(dname, fnames{i});
 	fprintf('%d: %s\n', i, fname);
 	I = imread(fname);
 	%I = imresize(I, 0.1);
@@ -62,15 +64,15 @@ end
 dists = zeros(length(fnames));
 num_pts = 50;
 
-%for i = 1:length(descriptors)
-	%for j = 1:length(descriptors)
-	i=1;j=2;
+for i = 1:length(descriptors)
+	for j = 1:length(descriptors)
+	%i=1;j=2;
 		fprintf('i=%d, j=%d: %s vs %s\n', i, j, fnames{i}, fnames{j});
 
 		[ matches, scores ] = vl_ubcmatch(descriptors{i}, descriptors{j});
 		scores = sort(scores, 'descend');
 		dists(1, i) = sum(scores(1:num_pts), 2);
-	%end
-%end
+	end
+end
 
-%save('./sift.mat');
+save('./sift.mat');
